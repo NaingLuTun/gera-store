f41ebaca820082a37743d65db8d2ddf464f0d212
Naing Lu Tun <nainglu7924@gmail.com>
2024-08-01 02:11:52 +0630

    implemented slider for home page top images

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,15 +1,17 @@
 import NavBar from "./nav-bar/NavBar"
-import topImgSliderTextile from "../assets/slider_moblie_textile_with_text.png"
+
 
 import "./css/HomePage.css"
+import ActiveSlider from "./ActiveSlider"
 function HomePage() {
   return (
     <div id="homePage">
         <NavBar />
 
-        <div className="homePageTopImgSlider">
-            <img src={topImgSliderTextile} alt="textile" className="homePageTopImageSliderImg"/>
-        </div>
+        <ActiveSlider/>
+            
+
+        <p>Dark green</p>
     </div>
   )
 }
